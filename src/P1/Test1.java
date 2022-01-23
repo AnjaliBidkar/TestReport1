7559d804b517074ccd78b283456319cfa94bc5e7
Anjali Bidkar <bidkaranjalibapurao@gmail.com>
2022-01-23 09:10:38 +0530

    ANjali

--- src/P1/Test1.java
+++ src/P1/Test1.java
@@ -3,8 +3,8 @@ package P1;
 public class Test1 {
 
 	public static void main(String[] args) {
-		System.out.println("In p1 Test1");
-
+		System.out.println("In p1 Test1 after b2");
+System.out.println("fddf");
 	}
 
 }
--- src/P2/Test1.java
+++ src/P2/Test1.java
@@ -4,7 +4,7 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		System.out.println("In p2 Test1");
-
+       System.out.println("hjbjbj");
 	}
 
 }
