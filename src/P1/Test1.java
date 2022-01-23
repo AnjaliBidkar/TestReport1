3bb46359cd231a2d35f228308ac556380c7499ee
Anjali Bidkar <bidkaranjalibapurao@gmail.com>
2022-01-23 10:13:41 +0530

    resolve confict

--- src/P1/Test1.java
+++ src/P1/Test1.java
@@ -3,8 +3,10 @@ package P1;
 public class Test1 {
 
 	public static void main(String[] args) {
-		System.out.println("In p1 Test1 after b2ccccccccccccccccccccccccc");
-System.out.println("fddf");
+		System.out.println("In p1 Test1 after b2,,,,,,,,,,,,,,,,,,,,,,,");
+        System.out.println("fddf");
+        System.out.println("In p1 Test1 after b2ccccccccccccccccccccccccc");
+        System.out.println("fddf");
 	}
 
 }
